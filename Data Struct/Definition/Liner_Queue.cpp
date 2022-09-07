6d186f27a16c98b2be4ec1472277c7c771a7cf30
785360050 <785360050@qq.com>
2022-09-07 19:18:58 +0800

    Initialize Data Struct

---
+++ Data Struct/Definition/Liner_Queue.cpp
@@ -0,0 +1,184 @@
+#include "C:/Users/len/Desktop/Data Struct/Data Struct/API/Liner_Queue.h"
+
+///顺序队列
+SeqQueue* SeqQueue_Init()
+{
+	SeqQueue* q = new SeqQueue;
+	if (q)
+	{
+		memset(q->data, 0, sizeof(q->data));
+		q->front = 0;
+		q->rear = 0;
+	}
+		
+	return q;
+}
+
+void SeqQueue_Destory(SeqQueue* SeqQueue)
+{
+	if (SeqQueue)
+		//if (SeqQueue->data)
+		//{
+		//	free(SeqQueue->data);
+		//}			
+		//此处应为非malloc申请的空间，而是定义的data[maxsize]，free(SeqQueue->data)后SeqQueue->data是野指针，所以无法free(SeqQueue)
+		free(SeqQueue);
+}
+
+bool SeqQueue_CheckFull(SeqQueue* SeqQueue)
+{
+	if ((SeqQueue->rear - SeqQueue->front + 1)%maxsize==SeqQueue->front)
+	{
+		return true;
+	}
+	return false;
+}
+
+void SeqQueue_Push(SeqQueue* SeqQueue, DataType data)
+{
+	try
+	{
+		if (!SeqQueue|| SeqQueue_CheckFull(SeqQueue))
+		{
+			throw 1;
+		}
+	}
+	catch (...)
+	{
+		std::cout << "SeqQueue is not exist || Queue is Full" << std::endl;
+		exit(0);
+	}
+	SeqQueue->data[SeqQueue->rear] = data;
+	SeqQueue->rear= (SeqQueue->rear + 1) % maxsize;
+
+}
+
+DataType SeqQueue_Pop(SeqQueue* SeqQueue)
+{
+	try
+	{
+		if (SeqQueue->front==SeqQueue->rear)
+		{
+			throw 1;
+		}
+	}
+	catch (...)
+	{
+		std::cout << "Queue is empty" << std::endl;
+		exit(0);
+	}
+	DataType t = SeqQueue->data[SeqQueue->front];
+	SeqQueue->front = (SeqQueue->front + 1) % maxsize;
+	return t;
+}
+
+
+
+///链式队列
+LinkQueue* LinkQueue_Init()
+{
+	LinkQueue* q = new LinkQueue;
+	QueNode* f = new QueNode;
+	f->data = 0;
+	f->next = NULL;
+	q->front = f;
+	q->rear = f;
+	q->length = 0;
+	return q;
+}
+
+void LinkQueue_Destroy(LinkQueue* LinkQueue)
+{
+	delete LinkQueue;
+}
+
+void LinkQueue_Push(LinkQueue* LinkQueue, DataType data)
+{
+	QueNode* n = new QueNode;
+	if (LinkQueue->length==0)
+	{
+		LinkQueue->rear->data = data;
+		LinkQueue->length++;
+	}
+	else
+	{
+		try
+		{
+			if (!n)
+			{
+				throw 1;
+			}
+		}
+		catch (...)
+		{
+			std::cout << "QueNode malloc failed,Push has disrupted"<<std::endl;
+			exit(0);
+		}
+		n->data = data;
+		n->next = NULL;
+		LinkQueue->rear->next = n;
+		LinkQueue->rear = n;
+		LinkQueue->length++;
+	}
+	
+}
+
+DataType LinkQueue_Pop(LinkQueue* LinkQueue)
+{
+	try
+	{
+		if (LinkQueue->length<=0)
+		{
+			throw 1;
+		}
+	}
+	catch (...)
+	{
+		std::cout << "Pop faild , LinkQueue is empty" << std::endl;
+		exit(0);
+	}
+	if (LinkQueue->length==1)
+	{
+		LinkQueue->length--;
+		return LinkQueue->front->data;
+	}
+	else
+	{
+		DataType t = LinkQueue->front->data;
+		QueNode* tn = LinkQueue->front;
+		LinkQueue->front = LinkQueue->front->next;
+		free(tn);
+		LinkQueue->length--;
+		return t;
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Data Struct/main.cpp
@@ -0,0 +1,126 @@
+﻿#include <iostream>
+#include "API/Liner_List_Link.h"
+#include "API/Liner_List_Sequential.h"
+#include "API/Liner_Stack.h"
+#include "API/Liner_Queue.h"
+
+
+
+void TestSeqList()
+{
+	SeqList* L = SeqList_Init(10);
+	SeqList_Show(L);
+	for(int i=1;i<=5;i++)
+		SeqList_Insert(L, i, i);
+	std::cout << SeqList_Delete(L, 3) << std::endl;
+	SeqList_Update(L, 9, 9);
+	SeqList_Show(L);
+	SeqList_Destroy(L);
+}
+
+void TestLinkList()
+{
+	/// <summary>
+	/// 测试链表的函数
+	/// </summary>
+	LinkList* L = LinkList_Init();
+	for(int i=1;i<=10;i++)
+		LinkList_AddNode_front(L, i);
+	//LinkList_AddNode_rear(L, e);
+	LinkList_Show(L);
+
+
+	LinkList_InsertNode(L, 2, 2);
+	LinkList_Show(L);
+	ListNode* s = (ListNode*)malloc(sizeof(ListNode));
+	ListNode* p = (ListNode*)malloc(sizeof(ListNode));
+	s = LinkList_LocateNode(L, 2);
+	std::cout << LinkList_GetElem(s) << std::endl;
+	p = LinkList_PriNode(L,s);
+	std::cout << LinkList_GetElem(p) << std::endl;
+	LinkList_DeleteList(L);
+
+}
+
+void TestSeqStack()
+{
+	SeqStack* s = SeqStack_Init(5);
+	for (int i = 0; i < StackSize; i++)
+	{
+		SeqStack_Push(s, i);
+	}
+	for (int i = 0; i < StackSize; i++)
+		std::cout << s->data[i] << " ";
+	std::cout << std::endl;
+	DataType x;
+	while(s->top>0)
+	{
+		x=SeqStack_Pop(s);
+		std::cout << x << " ";
+	}
+
+	SeqStack_Destroy(s);
+}
+void TestLinkStack()
+{
+	LinkStack* S = LinkStack_Init();
+	for (int i = 0; i < 5; i++)
+	{
+		LinkStack_Push(S, i);
+	}
+	LinkStack_Push(S, 1);
+	while (S->next)
+	{
+		std::cout << LinkStack_Pop(S) << std::endl;
+	}
+	LinkStack_Destroy(S);
+}
+
+void TestSeqQueue()
+{
+	SeqQueue* q;
+	q = SeqQueue_Init();
+	for (int i = 1; i < 6; i++)
+	{
+		SeqQueue_Push(q, i);
+	}
+	while (q->front!=q->rear)
+	{
+		std::cout << SeqQueue_Pop(q) << std::endl;
+	}
+	SeqQueue_Destory(q);
+
+}
+void TestLinkQueue()
+{
+	LinkQueue* q = LinkQueue_Init();
+	for (int i = 1; i <= 5; i++)
+	{
+		LinkQueue_Push(q, i);
+	}
+	while (q->length>0)
+	{
+		std::cout << LinkQueue_Pop(q)<<std::endl;
+	}
+	LinkQueue_Destroy(q);
+}
+
+
+void main()
+{
+	//TestSeqList();//完毕
+	//TestLinkList();//完毕
+	//TestSeqStack();//完毕
+	//TestLinkStack();//完毕
+	//TestSeqQueue();//完毕
+	//TestLinkQueue();//完毕
+
+
+	
+	
+	
+	
+}
+
+
+
