27ecd897e24354ccef64cecc00100843d9d8112c
Jevon <785360050@qq.com>
2023-09-27 09:26:48 +0800

    Test

--- Data Struct/C++/Liner_Structure/Liner_List/Liner_List_Sequential/Test.cpp
+++ Data Struct/C++/Liner_Structure/Liner_List/Liner_List_Sequential/Test.cpp
@@ -1,19 +1,15 @@
 #include <iostream>
 
-//#include "Liner_List_ADT.h"
+// #include "Liner_List_ADT.h"
 #include "Liner_List_Sequential.hpp"
 
-
-
-
-#ifndef __LINUX__
-#else
+#ifdef _WIN32
 #include <Windows.h>
-///修改cmd编码方式
-static bool  SetEncode(int EncodeId = 936)
+/// 修改cmd编码方式
+static bool SetEncode(int EncodeId = 936)
 {
 	/// <summary>
-	/// EncodeId 是代码页，设置它等于设置我们指定的编码格式。
+	/// EncodeId 是代码页,设置它等于设置我们指定的编码格式。
 	///	默认936为unicode编码
 	/// SetConsoleCP() 设置控制台输入时使用的编码。
 	///	SetConsoleOutputCP() 设置控制台输出时使用的编码。
@@ -30,37 +26,39 @@ void Test_SeqList_Array()
 	std::cout << "[Static Sequential List]" << std::endl;
 	Sequential_List_Array<int> L(5);
 	L.List_Show("初始化5个元素空间的顺序表后");
+	std::cout << std::endl;
 
 	for (int i = 1; i <= 5; i++)
 		L.Element_Insert(i, i);
 	L.List_Show("插入5个元素后");
+	std::cout << std::endl;
 
 	try
 	{
 		L.Element_Insert(2, 9);
 	}
-	catch(const std::exception& e)
+	catch (const std::exception &e)
 	{
 		std::cerr << e.what() << '\n';
 	}
-	
-	L.List_Show("第二个位置插入9后，检测到存储空间已满，报错");
 
+	L.List_Show("第二个位置插入9后,检测到存储空间已满,捕获异常");
 	std::cout << std::endl;
+
 	try
 	{
 		L.Element_Insert(20, 20);
 	}
-	catch(const std::exception& e)
+	catch (const std::exception &e)
 	{
 		std::cerr << e.what() << '\n';
 	}
-	L.List_Show("第20个非法位置插入20后，报错");
-
-	L.Element_Insert(20, 20);
+	L.List_Show("第20个非法位置插入20后,捕获异常");
 	std::cout << std::endl;
+
 	L.Element_Update(2, 999);
 	L.List_Show("第二个元素改为999后");
+	std::cout << std::endl;
 
 	int del;
 	del = L.Element_Delete(2);
@@ -74,7 +72,6 @@ void Test_SeqList_Array()
 	L.List_Clear();
 	L.List_Show("清空后");
 	std::cout << "当前表长：" << L.List_GetLength() << std::endl;
-
 }
 
 void Test_SeqList_Vector()
@@ -83,38 +80,42 @@ void Test_SeqList_Vector()
 	std::cout << "[Dynamic Sequential List]" << std::endl;
 	Sequential_List_Vector<int> L(5);
 	L.List_Show("初始化5个元素空间的顺序表后");
+	std::cout << std::endl;
 
 	for (int i = 1; i <= 5; i++)
 		L.Element_Insert(i, i);
 	L.List_Show("插入5个元素后");
+	std::cout << std::endl;
 
 	L.Element_Insert(2, 9);
-	L.List_Show("第二个位置插入9后，检测到存储空间已满，扩展空间");
+	L.List_Show("第二个位置插入9后,检测到存储空间已满,扩展空间");
+	std::cout << std::endl;
 
 	std::cout << std::endl;
 	L.Element_Insert(20, 20);
 	L.List_Show("第20个非法位置插入20后");
-
 	std::cout << std::endl;
 	L.Element_Update(2, 999);
 	L.List_Show("第二个元素改为999后");
+	std::cout << std::endl;
 
 	int del = L.Element_Delete(2);
 	L.List_Show("删除第二个元素后，自动收缩存储空间");
 	std::cout << "删除的第二个元素为:" << del << std::endl;
 
 	std::cout << "获取第二个元素值:" << L.List_GetData(2) << std::endl;
+	std::cout << std::endl;
 
 	for (size_t i = 5; i > 2; --i)
 		L.Element_Delete(i);
-	L.List_Show("删除第3个元素后,再次收缩存储空间");
+	L.List_Show("删除3个元素后,再次收缩存储空间");
+	std::cout << std::endl;
 
 	std::cout << "判空 :" << boolalpha << L.List_CheckEmpty() << std::endl;
 	std::cout << "当前表长：" << L.List_GetLength() << std::endl;
 	L.List_Clear();
 	L.List_Show("清空后");
 	std::cout << "当前表长：" << L.List_GetLength() << std::endl;
-
 }
 
 int main()
@@ -123,6 +124,3 @@ int main()
 	Test_SeqList_Array();
 	Test_SeqList_Vector();
 }
-
-
-
